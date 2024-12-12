4fff2b9eac0786390940fc475c585c01832d9bdb
divinesrkti2005 <158579254+divinesrkti2005@users.noreply.github.com>
2024-12-12 18:59:00 +0545

    commit

---
+++ lib/views/onboarding_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class OnboardingView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo.png', height: 200),
+            SizedBox(height: 30),
+            Text(
+              'Welcome to SneakBid',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Discover and Bid on Exclusive Sneakers',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Text('Get Started'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                textStyle: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
